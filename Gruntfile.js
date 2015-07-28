e5dd9037643b5b22556bf361833c315b38495885
S1SYPHOS <schmardt@posteo.se>
2015-07-28 14:06:34 +0200

    Added Modernizr + grunt-modernizr

--- Gruntfile.js
+++ Gruntfile.js
@@ -231,42 +231,85 @@ module.exports = function(grunt) {
     },
 
 
-    processhtml: {
-      dist:{
-        options: {
-          process: true,
-        },
-        files: [
-        {
-          expand: true,
-          cwd: 'build',
-          src: ['*.html'],
-          dest: 'build/',
-          ext: '.html'
-        },
-        ],
-      }
-    },
 
-    ftpush: {
-      build: {
-        auth: {
-          host: 'srv24.sysproserver.de',
-          port: 21,
-          authKey: 'elbhannover'
+
+
+    modernizr: {
+      dist: {
+        "devFile" : "bower_components/modernizr/modernizr.js",
+        "outputFile" : "build/js/modernizr-custom.js",
+        // Based on default settings on http://modernizr.com/download/
+        "extra" : {
+            "shiv" : true,
+            "printshiv" : false,
+            "load" : true,
+            "mq" : false,
+            "cssclasses" : true
         },
-        src: '/home/arch-user/Documents/NewProjects/zak/build',
-        dest: '/html/development',
-        // exclusions: ['/html/development/img/fav/*.png'],
-        keep: ['/html/development/img/*.jpg', '/html/development/img/fav/*.png'],
-        simple: false,
-        useList: false
+        // Based on default settings on http://modernizr.com/download/
+        "extensibility" : {
+          "addtest" : false,
+          "prefixed" : false,
+          "teststyles" : false,
+          "testprops" : false,
+          "testallprops" : false,
+          "hasevents" : false,
+          "prefixes" : false,
+          "domprefixes" : false,
+          "cssclassprefix": ""
+        },
+        // By default, source is uglified before saving
+        "uglify" : true,
+        // Define any tests you want to implicitly include.
+        "tests" : [],
+        // By default, this task will crawl your project for references to Modernizr tests.
+        // Set to false to disable.
+        "parseFiles" : false,
+        // When parseFiles = true, this task will crawl all *.js, *.css, *.scss and *.sass files,
+        // except files that are in node_modules/.
+        // You can override this by defining a "files" array below.
+        files: {
+          src: [
+            'build/js/**/*.js',
+            'build/css/style.css',
+          ]
+        },
+        // This handler will be passed an array of all the test names passed to the Modernizr API, and will run after the API call has returned
+        // "handler": function (tests) {},
+
+        // When parseFiles = true, matchCommunityTests = true will attempt to
+        // match user-contributed tests.
+        "matchCommunityTests" : false,
+        // Have custom Modernizr tests? Add paths to their location here.
+        "customTests" : []
+        }
       }
-    }
+
+
+
+
+
 
 
 
       /* UNUSED STUFF
+
+      processhtml: {
+        dist:{
+          options: {
+            process: true,
+          },
+          files: [
+          {
+            expand: true,
+            cwd: 'build',
+            src: ['*.html'],
+            dest: 'build/',
+            ext: '.html'
+          },
+          ],
+        }
+      },
 
       dataUri: {
         dist: {
@@ -300,6 +343,7 @@ module.exports = function(grunt) {
     grunt.registerTask('dev', [
       'sass',
       'jekyll:build',
+      'modernizr',
       'browserSync',
     	'watch'
     ]);
@@ -308,9 +352,10 @@ module.exports = function(grunt) {
       'sass',
       'jekyll:build',
       'uncss',
+      'modernizr',
       'autoprefixer',
       'csscomb',
-      'cssmin:all',
+      'cssmin',
       'useminPrepare',
       'concat',
       'uglify',
